4f73c3f8c6a7bf28a299c931cf96214232e565f7
abhishek-goyal <45648611+Abhishek445566@users.noreply.github.com>
2024-07-25 16:18:30 +0100

    Creating 3 models and computing average of accuracy.py

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ from preprocess import *
 from embeddings import *
 from modelling.modelling import *
 from modelling.data_model import *
-import numpy as np
-import pandas as pd
 import random
 seed =0
-random.seed(seed) #This sets the seed for Python's built-in random module.
-np.random.seed(seed) #This sets the seed for NumPy's random number generator.
+random.seed(seed)
+np.random.seed(seed)
+
 
 def load_data():
     #load the input data
     df = get_input_data()
+    df["y"] = df[Config.CLASS_COL]
+    df = df.loc[(df["y"] != '') & (~df["y"].isna()),]
+    return  df
+
+def load_data_for_type3():
+    df = get_input_data()
+    df["y"] = df[Config.TYPE_COLS[2]]
+    df = df.loc[(df["y"] != '') & (~df["y"].isna()),]
+    return  df
+
+def load_data_for_type4():
+    df = get_input_data()
+    df["y"] = df[Config.TYPE_COLS[2]]
+    df = df.loc[(df["y"] != '') & (~df["y"].isna()),]
     return  df
 
 def preprocess_data(df):
@@ -31,11 +44,11 @@ def get_data_object(X: np.ndarray, df: pd.DataFrame):
     return Data(X, df)
 
 def perform_modelling(data: Data, df: pd.DataFrame, name):
-    model_predict(data, df, name)
+    return model_predict(data, df, name)
 
-if __name__ == '__main__':
-    df = load_data()
+def type_prediction(df):
     df = preprocess_data(df)
+   
     df[Config.INTERACTION_CONTENT] = df[Config.INTERACTION_CONTENT].values.astype('U')
     df[Config.TICKET_SUMMARY] = df[Config.TICKET_SUMMARY].values.astype('U')
     grouped_df = df.groupby(Config.GROUPED)
@@ -43,5 +56,23 @@ if __name__ == '__main__':
         print(name)
         X, group_df = get_embeddings(group_df)
         data = get_data_object(X, group_df)
-        perform_modelling(data, group_df, name)
+        type_score , predictions=perform_modelling(data, group_df, name)
+        return type_score, predictions
 
+
+if __name__ == '__main__':
+    df=load_data()
+    df_for_type3=load_data_for_type3()
+    df_for_type4=load_data_for_type4()
+    print("Prediction Type 2")
+    type2 , prediction2=type_prediction(df)
+    print("Prediction Type 3")
+    type3 , prediction3=type_prediction(df_for_type3)
+    print("Prediction Type 4")
+    type4 , prediction4=type_prediction(df_for_type4)
+    print(f"Prediction of type 2= {type2}")
+    average=(type2+type3+type4)/3
+    print(f"average = {average}")
+    print(prediction2)
+    
+    
